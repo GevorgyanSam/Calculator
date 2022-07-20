ebd1c13949bd3bcd0c4c80e53ef57e0073355c3a
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-07-20 19:44:02 +0400

    Calculator Light & Dark Mode

---
+++ js/script.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const display = document.getElementById("result");
+const darkTheme = document.querySelector("img");
+
+darkTheme.addEventListener("click", darkThemeFunc);
+
+function darkThemeFunc(){
+    if(darkTheme.src = "img/sun.jpg"){
+        document.body.classList.toggle('dark');
+        darkTheme.src = "img/moon.png";
+    }
+    else if(darkTheme.src = "img/moon.png"){
+        document.body.classList.toggle('');
+        darkTheme.src = "img/sun.jpg";
+    }
+}
+
+function num(x) {
+    switch (x) {
+        case 1:
+            display.value += "1";
+            break;
+        case 2:
+            display.value += "2";
+            break;
+        case 3:
+            display.value += "3";
+            break;
+        case 4:
+            display.value += "4";
+            break;
+        case 5:
+            display.value += "5";
+            break;
+        case 6:
+            display.value += "6";
+            break;
+        case 7:
+            display.value += "7";
+            break;
+        case 8:
+            display.value += "8";
+            break;
+        case 9:
+            display.value += "9";
+            break;
+        case 0:
+            display.value += "0";
+            break;
+    }
+}
+
+function operator(op) {
+    switch (op) {
+        case "+":
+            display.value += "+";
+            break;
+        case "-":
+            display.value += "-";
+            break;
+        case "/":
+            display.value += "/";
+            break;
+        case "*":
+            display.value += "*";
+            break;
+        case ".":
+            display.value += ".";
+            break;
+    }
+}
+
+function clearAll(){
+    display.value = "";
+}
+
+function final(){
+    display.value = +eval(display.value);
+}
